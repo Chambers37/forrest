23e70104d4966f85115113624304cff4d71cc302
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-01-15 22:14:29 -0500

    added homepage image

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,16 @@
+'use client'
 
+import Image from "next/image";
 
 export default function Homepage() {
   return (
     <div>
+      <Image
+        src='/images/pool-banner-cropped.png'
+        width={4000}
+        height={1100}
+        alt='Pool Deck and Retaining Wall'
+      />
       <p>Home</p>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,27 +10,28 @@ export default function Nabar() {
 
   return (
     <main className='flex flex-col items-center bg-gray-100 py-4'>
-      <Image src="/images/site-logo.png"
+      <Image
+        src="/images/site-logo.png"
         width={300}
         height={96}
         alt="Forrest Hardscape and Landscape"
         className="h-[96px] w-[300px] object-contain block"
       />
       
-      <nav className='flex space-x-6'>
-        <a href="/" className={`text-2xl font-Merriweather font-medium italic transition duration-200 ${
+      <nav className='flex flex-wrap justify-center text-center space-x-4'>
+        <a href="/" className={`text-2xl font-Merriweather-Bold font-bold italic transition duration-200 ${
             pathname === '/' ? 'text-black' : 'text-green-800 hover:text-lightGreen'
           }`}>Home</a>
         <span className='text-gray-400'>|</span>
-        <a href="/services" className={`text-2xl font-Merriweather font-medium italic transition duration-200 ${
+        <a href="/services" className={`text-2xl font-Merriweather font-bold italic transition duration-200 ${
             pathname === '/services' ? 'text-black' : 'text-green-800 hover:text-lightGreen'
           }`}>Our Services</a>
         <span className='text-gray-400'>|</span>
-        <a href="/quote" className={`text-2xl font-Merriweather font-medium italic transition duration-200 ${
+        <a href="/quote" className={`text-2xl font-Merriweather font-bold italic transition duration-200 ${
             pathname === '/quote' ? 'text-black' : 'text-green-800 hover:text-lightGreen'
           }`}>Request A Quote</a>
         <span className='text-gray-400'>|</span>
-        <a href="/about" className={`text-2xl font-Merriweather font-medium italic transition duration-200 ${
+        <a href="/about" className={`text-2xl font-Merriweather font-bold italic transition duration-200 ${
             pathname === '/about' ? 'text-black' : 'text-green-800 hover:text-lightGreen'
           }`}>About Us</a>
       </nav>
