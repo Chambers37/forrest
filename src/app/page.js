5376c7c66e45d607f7659f8827bcb9d7f34da2b7
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-01-16 23:21:54 -0500

    added title banner? large box with text above image

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,25 @@ import Image from "next/image";
 export default function Homepage() {
   return (
     <main>
-      
-      <div className="relative w-full max-w-[4000px] h-[500px] mx-auto overflow-hidden bg-red-500">
+      <div className="bg-lightGreen w-full text-black text-center py-4">
+        <p className="text-6xl font-bold">
+        Professional
+        </p>
+        <p className="text-6xl font-bold">
+        
+        Hardscape & Landscape
+        </p>
+        <p className="text-6xl font-bold">
+        Services
+        </p>
+      </div>
+      <div className="relative w-full max-w-[4000px] h-[500px] mx-auto overflow-hidden">
 
         <Image
           src='/images/pool-banner-cropped2.png'
           fill
           alt='Pool Deck and Retaining Wall'
-          className="object-cover p-10"
+          className="object-cover p-7"
           />
 
         </div>
