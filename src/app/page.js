a032c6755be1bda2c82b9ba211f086598f757786
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-01-16 23:06:38 -0500

    added home page banner photo and styled

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,21 @@ import Image from "next/image";
 
 export default function Homepage() {
   return (
-    <div>
-      <Image
-        src='/images/pool-banner-cropped.png'
-        width={4000}
-        height={1100}
-        alt='Pool Deck and Retaining Wall'
-      />
+    <main>
+      
+      <div className="relative w-full max-w-[4000px] h-[500px] mx-auto overflow-hidden bg-red-500">
+
+        <Image
+          src='/images/pool-banner-cropped2.png'
+          fill
+          alt='Pool Deck and Retaining Wall'
+          className="object-cover p-10"
+          />
+
+        </div>
+
       <p>Home</p>
-    </div>
+
+    </main>
   );
 }
