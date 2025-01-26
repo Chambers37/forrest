53c6100762726d056b2ab96e225874eed75dcdbe
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-01-26 16:22:13 -0500

    removed form border

--- src/app/quote/page.js
+++ src/app/quote/page.js
@@ -49,7 +49,7 @@ export default function Quote() {
         {/*Quote Form*/}
         <div className="fade-hidden flex w-full mx-auto p-3 items-center justify-center">
 
-          <form className="max-w-lg w-full text-2xl grid grid-cols-1 md:grid-cols-2 gap-7 border-black border-lg border-solid m-20">
+          <form className="max-w-lg w-full text-2xl grid grid-cols-1 md:grid-cols-2 gap-7 m-20">
             
             <div>
               <label htmlFor="first-name" className="block text-gray-700">
@@ -102,7 +102,7 @@ export default function Quote() {
               />        
             </div>
           
-            <div>
+            <div className="flex flex-col">
               <label htmlFor="Phone" className="block text-gray-700">
                 Phone
               </label>
