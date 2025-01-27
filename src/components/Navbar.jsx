fe28375e03dc38ddf1df8c79585833feda279fb3
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-01-26 21:28:29 -0500

    Improved navbar styling, added scale effect and aligned color scheme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,33 +24,33 @@ export default function Nabar() {
 
       <div className='flex flex-row-reverse'>
 
-        <button className='hover:bg-green-800 hover:border-white hover:text-lightGreen hover:scale-110 hover:border-green-800 hover:text-white transition duration-300 self-end rounded-lg bg-lightGreen border-lightGreen border-solid border-2 p-1 mr-2 md:mr-3 lg:mr-5 py-2 mb-1'>
+        <button className='hover:bg-darkGreen hover:border-white hover:text-greenWhite hover:scale-110 transition duration-300 self-end rounded-lg bg-lightGreen border-lightGreen border-solid border-2 px-1 mr-2 md:mr-3 lg:mr-5 py-2 mb-1'>
           <p>Call Today</p>
         </button>
 
         <nav className='flex flex-wrap items-end text-center space-x-3 mr-2 md:mr-3 lg:mr-5 pb-1'>
 
-          <a href="/" className={`text-l font-Merriweather-Bold font-bold italic transition duration-200 ${
+          <a href="/" className={`text-l font-Merriweather hover:scale-110 font-bold italic transition duration-200 ${
             pathname === '/' ? 'text-black' : 'text-green-800 hover:text-lightGreen'
           }`}>Home</a>
 
           <span className='text-gray-400'>|</span>
-          <a href="/services" className={`text-l font-Merriweather font-bold italic transition duration-200 ${
+          <a href="/services" className={`text-l font-Merriweather hover:scale-110 font-bold italic transition duration-200 ${
             pathname === '/services' ? 'text-black' : 'text-green-800 hover:text-lightGreen'
           }`}>Our Services</a>
 
           <span className='text-gray-400'>|</span>
-          <a href="/quote" className={`text-l font-Merriweather font-bold italic transition duration-200 ${
+          <a href="/quote" className={`text-l font-Merriweather hover:scale-110 font-bold italic transition duration-200 ${
             pathname === '/quote' ? 'text-black' : 'text-green-800 hover:text-lightGreen'
           }`}>Request A Quote</a>
 
           <span className='text-gray-400'>|</span>
-          <a href="/portfolio" className={`text-l font-Merriweather font-bold italic transition duration-200 ${
+          <a href="/portfolio" className={`text-l font-Merriweather hover:scale-110 font-bold italic transition duration-200 ${
             pathname === '/portfolio' ? 'text-black' : 'text-green-800 hover:text-lightGreen'
           }`}>Portfolio</a>
 
             <span className='text-gray-400'>|</span>
-          <a href="/about" className={`text-l font-Merriweather font-bold italic transition duration-200 ${
+          <a href="/about" className={`text-l font-Merriweather hover:scale-110 font-bold italic transition duration-200 ${
             pathname === '/about' ? 'text-black' : 'text-green-800 hover:text-lightGreen'
           }`}>About Us</a>
 
