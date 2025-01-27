07c0baf6c50088aa054c5e4711f9afbdd9fa747b
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-01-26 21:17:07 -0500

    Added content to about us and restructered layout

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,8 +3,10 @@ import Image from "next/image";
 export default function About() {
   return (
     <div className="grid grid-cols-1">
+
+      {/* About Us Text */}
       <section>
-        <div className="justify-between grid grid-cols-1 md:grid-cols-2">
+        <div className="grid grid-cols-1 md:grid-cols-2">
           <div className="text-6xl text-darkGreen flex justify-center items-center pt-4"><h1>About Us</h1></div>
           <div className="py-4">
             <div className="p-3 md:pb-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tenetur voluptatem repudiandae dolorum aperiam, beatae, consectetur officia exercitationem repellendus officiis, delectus deserunt? Perferendis facilis ea fuga impedit, quae quos reiciendis!
@@ -45,17 +47,39 @@ export default function About() {
         </div>
         </div>
       </section>
-      <section>
-        <div className="grid grid-cols-1 md:grid-cols-2">
 
-        </div>meet the team text
-      </section>
+      {/* Meet The Team */}
       <section>
-        <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className="grid grid-cols-1 w-full">
+          <div className="text-center text-5xl text-darkGreen font-medium pt-10">
+            <h1>Meet The Team</h1>
+          </div>
+          <div className="text-center text-xl text-darkGreen font-medium pt-5">
+            <p>Matt and his team have a combined 35+ years <br /> of experience in the landscape field!</p>
+          </div>
+        </div>
 
-        </div>team photos
-      </section>
-      <section>
+      {/* Team Photos */}
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-center mx-auto max-w-5xl">
+          <div className="py-2 flex justify-center">
+            <Image
+              src='/images/service-landscape.png'
+              width={300}
+              height={500}
+              alt='test'
+            />
+          </div>
+          <div className="py-2 flex justify-center">
+            <Image
+              src='/images/service-landscape.png'
+              width={300}
+              height={500}
+              alt='test'
+            />
+          </div>
+        </div>
+
+      {/* Team Names + Text */}
         <div className="grid grid-cols-1 md:grid-cols-2">
 
         </div>team member text
