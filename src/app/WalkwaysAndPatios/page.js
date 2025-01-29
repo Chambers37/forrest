251fa6efad8dad6c32aedc4642708832cd687ebd
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-01-29 18:05:09 -0500

    testing service page layouts

--- src/app/WalkwaysAndPatios/page.js
+++ src/app/WalkwaysAndPatios/page.js
@@ -1,9 +1,20 @@
 import Link from "next/link";
+import Image from "next/image"
 
 
 export default function WalkwaysAndPatios() {
   return (
     <div>
+      <section>
+        <div className="relative w-full max-w-[4000px] h-[300px] overflow-hidden">
+          <Image
+            src='/images/services-banner-cropped.png'
+            fill
+            alt='Pool Deck and Retaining Wall'
+            className="object-cover"
+          />
+        </div>
+      </section>
       <div>
         <div className="h-[75vh] items-center flex justify-center">
           <div className="justify-center flex items-center">
