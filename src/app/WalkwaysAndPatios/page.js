f99cec2a28ab8ef8ebd7beaca6696a90610d1fa2
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-02-04 18:45:56 -0500

    restyled button

--- src/app/WalkwaysAndPatios/page.js
+++ src/app/WalkwaysAndPatios/page.js
@@ -57,7 +57,7 @@ export default function WalkwaysAndPatios() {
         {/* Back to Services Button */}
         <section className="flex justify-center my-4">
           <Link href='/Services'>
-            <button className="text-lg text-darkGreen hover:text-greenWhite hover:bg-darkGreen bg-transparent outline-dashed outline-greenWhite px-3 py-1">
+            <button className="text-lg text-darkGreen bg-transparent underline hover:outline-dashed hover:outline-black hover:outline-1 rounded px-1 py-1">
               Back To Services
             </button>
           </Link>
