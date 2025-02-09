ae8885fd66f269bd5755adbf0d900c1720b2e9c9
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-02-08 20:35:00 -0500

    Added layout to year round property care

--- src/app/WalkwaysAndPatios/page.js
+++ src/app/WalkwaysAndPatios/page.js
@@ -85,7 +85,7 @@ export default function WalkwaysAndPatios() {
         {/* Service Detail Section */}
         <section className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full fade-hidden">
           <div>
-            <div className="bg-red-300 text-4xl p-2"><h1>Service Title</h1></div>
+            <div className="bg-red-300 text-4xl p-2"><h1>Walkways And Patios</h1></div>
             <div className="bg-blue-300 text-lg p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid ullam quae eius possimus saepe veritatis, totam ipsum cumque repellendus quo accusamus temporibus dicta! Ad labore magnam, voluptatibus veniam officiis voluptate.</div>
             <div className="bg-red-300 text-3xl p-2">mini title</div>
             <div className="bg-blue-300 text-lg p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid ullam quae eius possimus saepe veritatis, totam ipsum cumque repellendus quo accusamus temporibus dicta! Ad labore magnam, voluptatibus veniam officiis voluptate.</div>
