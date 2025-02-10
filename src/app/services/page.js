83ba45c1dee898419935879d71bb53429d13ccbb
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-02-10 18:20:49 -0500

    Fixed Banner Text on Main services page to stay centered always

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -26,7 +26,7 @@ export default function Services() {
             className="object-cover"
           />
         </div>
-        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-bold text-7xl">
+        <h1 className="text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-bold text-7xl">
           Available Services
         </h1>
       </section>
