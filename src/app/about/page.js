006048548ac771b5ee1931b5026201253b0ba0a5
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-02-13 21:13:11 -0500

    Added more team member card placeholders

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -21,27 +21,27 @@ export default function About() {
 
       {/* About Us Text */}
       <section className="flex flex-col justify-center">
-        <div className="text-center  justify-center mx-auto max-w-screen-2xl grid grid-cols-1 md:grid-cols-2">
+        <div className="mt-5 text-center  justify-center mx-auto max-w-screen-2xl grid grid-cols-1 md:grid-cols-2">
           <div className="flex justify-center w-full">
-            <div className="text-6xl text-darkGreen w-4/5 flex justify-center items-center pt-4">
+            <div className="text-8xl text-darkGreen w-4/5 flex justify-center font-medium items-center pt-4">
               <h1>About Us</h1>
             </div>
           </div>
             <div className="py-4">
-              <div className="p-3 md:pb-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tenetur voluptatem repudiandae dolorum aperiam, beatae, consectetur officia exercitationem repellendus officiis, delectus deserunt? Perferendis facilis ea fuga impedit, quae quos reiciendis!
+              <div className="px-3 pb-7 mr-10">Forrest Hardscape & Landscape is a dedicated team of skilled professionals with a deep-rooted passion for transforming outdoor spaces, bringing generations of expertise in hardscaping, landscaping, and horticultural craftsmanship to every project.
               </div>
-              <div className="px-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tenetur voluptatem repudiandae dolorum aperiam, beatae, consectetur officia exercitationem repellendus officiis, delectus deserunt? Perferendis facilis ea fuga impedit, quae quos reiciendis!
+              <div className="px-3 pb-7 mr-10">With over <strong>100</strong> years of combined experience, we are committed to quality, precision, and customer satisfaction, the team works collaboratively to design, build, and maintain stunning landscapes that enhance the beauty, functionality, and value of residential and commercial properties across Chester County and beyond.
               </div>
               <div></div>
             </div>
         </div>
         {/* Images */}
         <div>
-          <div className="grid grid-cols-1 md:grid-cols-3">
+          <div className="grid grid-cols-1 gap-3 mx-2 sm:grid-cols-3">
           <div className="flex justify-center py-2 md:justify-end">
             <Image
               src='/images/service-landscape.png'
-              width={300}
+              width={400}
               height={500}
               alt='test'
             />
@@ -49,7 +49,7 @@ export default function About() {
           <div className="flex justify-center py-2 md:justify-center">
             <Image
               src='/images/testing.gif'
-              width={300}
+              width={400}
               height={500}
               alt='test'
             />
@@ -57,7 +57,7 @@ export default function About() {
           <div className="flex justify-center py-2 md:justify-start">
             <Image
               src='/images/service-landscape.png'
-              width={300}
+              width={400}
               height={500}
               alt='test'
             />
@@ -78,30 +78,112 @@ export default function About() {
           </div>
         </div>
 
-      {/* Team Photos */}
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-center mx-auto max-w-5xl">
-          <div className="py-2 flex justify-center">
-            <Image
-              src='/images/service-landscape.png'
-              width={300}
-              height={500}
-              alt='test'
-            />
+        {/* Team Photos */}
+        <div className="my-8 grid grid-cols-1 md:grid-cols-3 justify-center mx-auto max-w-screen-2xl gap-6"> 
+          {/* Matt Siter */}
+          <div className="flex flex-col h-full bg-white shadow-2xl">
+            <div className="h-[400px] flex justify-center">
+              <Image
+                src='/images/matt.jpg'
+                width={300}
+                height={400}
+                alt='Matthew Forrest Siter'
+                className="h-full object-cover"
+              />
+            </div>
+            <div className="px-5 py-4 flex-grow flex flex-col">
+              <h1 className="text-center text-4xl font-medium text-darkGreen">
+                Matthew Forrest Siter
+              </h1>
+              <h2 className="text-center font-medium">Owner / Operator</h2>
+              <hr className="my-4 border-black" />
+              <p className="indent-8 text-xl mb-4 pr-6 flex-grow">
+                Matt Siter is the owner of Forrest Hardscape & Landscape. Born and raised in Chester County, Matt is the fourth generation of his family to be involved in the horticultural field. He has been laying bricks, digging shrubs, and planting trees since he could hold a shovel.
+              </p>
+              <p className="indent-8 text-xl pr-6">
+                After graduating from Downingtown West High School, Matt attended Delaware Valley College (now Delaware Valley University) in Doylestown, Pennsylvania. He graduated with a bachelor’s degree in Ornamental Horticulture with a concentration in Landscape Contracting & Management in 2014. Throughout high school, college and afterwards, Matt gained experience working for multiple nurseries and landscape companies, where he learned to manage multiple crews and complete large scale projects. In 2019, he started his own hardscape and landscape company, Forrest Hardscape.
+              </p>
+            </div>
           </div>
-          <div className="py-2 flex justify-center">
-            <Image
-              src='/images/service-landscape.png'
-              width={300}
-              height={500}
-              alt='test'
-            />
+
+          {/* Maureciio */}
+          <div className="flex flex-col h-full bg-white shadow-2xl">
+            <div className="h-[400px] flex justify-center">
+              <Image
+                src='/images/matt.jpg'
+                width={300}
+                height={400}
+                alt='Maureciio Dude'
+                className="h-full object-cover"
+              />
+            </div>
+            <div className="px-5 py-4 flex-grow flex flex-col">
+              <h1 className="text-center text-4xl font-medium text-darkGreen">
+                Maureciio Dude
+              </h1>
+              <h2 className="text-center font-medium">Owner / Operator</h2>
+              <hr className="my-4 border-black" />
+              <p className="indent-8 text-xl mb-4 pr-6">
+                Matt Siter is the owner of Forrest Hardscape & Landscape. Born and raised in Chester County, Matt is the fourth generation of his family to be involved in the horticultural field. He has been laying bricks, digging shrubs, and planting trees since he could hold a shovel.
+              </p>
+              <p className="indent-8 text-xl pr-6">
+                After graduating from Downingtown West High School, Matt attended Delaware Valley College (now Delaware Valley University) Hardscape.
+              </p>
+            </div>
+          </div>
+
+          {/* Jeff */}
+          <div className="flex flex-col h-full bg-white shadow-2xl">
+            <div className="h-[400px] flex justify-center">
+              <Image
+                src='/images/matt.jpg'
+                width={300}
+                height={400}
+                alt='Maureciio Dude'
+                className="h-full object-cover"
+              />
+            </div>
+            <div className="px-5 py-4 flex-grow flex flex-col">
+              <h1 className="text-center text-4xl font-medium text-darkGreen">
+                Maureciio Dude
+              </h1>
+              <h2 className="text-center font-medium">Owner / Operator</h2>
+              <hr className="my-4 border-black" />
+              <p className="indent-8 text-xl mb-4 pr-6">
+                Matt Siter is the owner of Forrest Hardscape & Landscape. Born and raised in Chester County, Matt is the fourth generation of his family to be involved in the horticultural field. He has been laying bricks, digging shrubs, and planting trees since he could hold a shovel.
+              </p>
+              <p className="indent-8 text-xl pr-6">
+                After graduating from Downingtown West High School, Matt attended Delaware Valley College (now Delaware Valley University) Hardscape.
+              </p>
+            </div>
+          </div>
+
+          {/* Jeff Again */}
+          <div className="flex flex-col h-full bg-white shadow-2xl">
+            <div className="h-[400px] flex justify-center">
+              <Image
+                src='/images/matt.jpg'
+                width={300}
+                height={400}
+                alt='Matthew Forrest Siter'
+                className="h-full object-cover"
+              />
+            </div>
+            <div className="px-5 py-4 flex-grow flex flex-col">
+              <h1 className="text-center text-4xl font-medium text-darkGreen">
+                Matthew Forrest Siter
+              </h1>
+              <h2 className="text-center font-medium">Owner / Operator</h2>
+              <hr className="my-4 border-black" />
+              <p className="indent-8 text-xl mb-4 pr-6 flex-grow">
+                Matt Siter is the owner of Forrest Hardscape & Landscape. Born and raised in Chester County, Matt is the fourth generation of his family to be involved in the horticultural field. He has been laying bricks, digging shrubs, and planting trees since he could hold a shovel.
+              </p>
+              <p className="indent-8 text-xl pr-6">
+                After graduating from Downingtown West High School, Matt attended Delaware Valley College (now Delaware Valley University) in Doylestown, Pennsylvania. He graduated with a bachelor’s degree in Ornamental Horticulture with a concentration in Landscape Contracting & Management in 2014. Throughout high school, college and afterwards, Matt gained experience working for multiple nurseries and landscape companies, where he learned to manage multiple crews and complete large scale projects. In 2019, he started his own hardscape and landscape company, Forrest Hardscape.
+              </p>
+            </div>
           </div>
         </div>
-
-      {/* Team Names + Text */}
-        <div className="grid grid-cols-1 md:grid-cols-2">
-
-        </div>team member text
       </section>
     </div>
   );
