492c8b1df3d8a6be0dac8a98ec8973e5c0eceb92
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-02-23 20:24:10 -0500

    Adding list of types to stone work page

--- src/app/NaturalStoneWork/page.js
+++ src/app/NaturalStoneWork/page.js
@@ -85,10 +85,22 @@ export default function NaturalStoneWork() {
         {/* Service Detail Section */}
         <section className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full fade-hidden">
           <div>
-            <div className="bg-red-300 text-4xl p-2"><h1>Natural Stone Work</h1></div>
-            <div className="bg-blue-300 text-lg p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid ullam quae eius possimus saepe veritatis, totam ipsum cumque repellendus quo accusamus temporibus dicta! Ad labore magnam, voluptatibus veniam officiis voluptate.</div>
-            <div className="bg-red-300 text-3xl p-2">mini title</div>
-            <div className="bg-blue-300 text-lg p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid ullam quae eius possimus saepe veritatis, totam ipsum cumque repellendus quo accusamus temporibus dicta! Ad labore magnam, voluptatibus veniam officiis voluptate.</div>
+            <div className="text-4xl p-2"><h1>Natural Stone Work</h1></div>
+            <div className="text-lg p-2">
+              <p>
+                Like the look of natural stones in your garden space? Trust our experienced masons to continue the old-world traditions of proper and permeant stonework. 
+              </p>
+            </div>
+            <div>
+              <ul>
+                <li>Stucco</li>
+                <li>Full bed stonework</li>
+                <li>Veneer stonework</li>
+                <li>Brick work</li>
+                <li>Traditional bluestone </li>
+                <li>Traditional bluestone </li>
+              </ul>
+            </div>
           </div>
 
           {/* Image Section */}
@@ -104,15 +116,15 @@ export default function NaturalStoneWork() {
           </div>
         </section>
 
-        <div className="mb-5 fade-hidden">
-          <div className="bg-red-300 text-3xl p-2">mini title 2</div>
-          <div className="bg-blue-300 text-lg p-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit qui aut tempore nemo modi pariatur ad, cum ab aliquid nobis? In soluta reprehenderit vitae iure repudiandae voluptatem sunt atque dolores.</div>
-        </div>
+        {/* <div className="mb-5 fade-hidden">
+          <div className="text-3xl p-2">mini title 2</div>
+          <div className="text-lg p-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit qui aut tempore nemo modi pariatur ad, cum ab aliquid nobis? In soluta reprehenderit vitae iure repudiandae voluptatem sunt atque dolores.</div>
+        </div> */}
 
         <div>
 
           {/* Image Gallery */}
-          <section className="w-full fade-hidden">
+          <section className="w-full fade-hidden mt-5">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
               {images.map((srcUrl, i) => (
                 <div key={i} className="flex justify-center">
