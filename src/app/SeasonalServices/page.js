84fd3778708796ade8ee02749efcfc0a4d4d095b
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-03-14 22:46:34 -0400

    Updated images for seasonal services and added the modal

--- src/app/SeasonalServices/page.js
+++ src/app/SeasonalServices/page.js
@@ -12,30 +12,15 @@ export default function SeasonalServices() {
         const observer = applyFadeInEffect('fade-hidden');
   
         return () => observer.disconnect();
-    }, [])
-
-  const images = [
-    "/images/better-pool-banner.jpg",
-    "/images/services-banner.jpg",
-    "/images/better-pool-banner.jpg",
-    "/images/services-banner.jpg",
-    "/images/img3.jpg",
-    "/images/services-banner.jpg",
-    "/images/better-pool-banner.jpg",
-    "/images/services-banner.jpg",
-    "/images/better-pool-banner.jpg",
-  ];
+    }, []);
 
   const enlargedImages = [
-    "/images/better-pool-banner.jpg",
-    "/images/services-banner.jpg",
-    "/images/better-pool-banner.jpg",
-    "/images/services-banner.jpg",
-    "/images/img3.jpg",
-    "/images/services-banner.jpg",
-    "/images/better-pool-banner.jpg",
-    "/images/services-banner.jpg",
-    "/images/better-pool-banner.jpg",
+    '/images/landscape/seasonal_services/mulch/mulched_spring_bed.jpg',
+    '/images/landscape/seasonal_services/mulch/spring_tulips.jpg',
+    '/images/landscape/seasonal_services/leaf/leaf_blowing.jpg',
+    '/images/landscape/seasonal_services/leaf/leaf_cleanup.jpg',
+    '/images/landscape/seasonal_services/summer_pruning/front_walkway.jpg',
+    '/images/landscape/seasonal_services/summer_pruning/front_walk_with_bushes.jpg',
   ];
 
   const [isOpen, setIsOpen] = useState(false);
@@ -118,22 +103,64 @@ export default function SeasonalServices() {
               </div>
             </div>
             <div className="p-2 flex items-center">
-              <div className="relative w-full h-[300px]">
+              <div className="relative w-full h-[300px] hover:cursor-zoom-in" onClick={() => openModal(0)}>
                 <Image
-                  src='/images/services-banner-cropped.png'
+                  src='/images/landscape/seasonal_services/mulch/mulched_spring_bed.jpg'
                   fill
                   alt='Pool Deck and Retaining Wall'
-                  className="object-cover"
+                  className="object-cover hover:opacity-70"
                 />
               </div>
             </div>
             <div className="p-2 flex items-center">
-              <div className="relative w-full h-[300px]">
+              <div className="relative w-full h-[300px] hover:cursor-zoom-in" onClick={() => openModal(1)}>
                 <Image
-                  src='/images/services-banner-cropped.png'
+                  src='/images/landscape/seasonal_services/mulch/spring_tulips.jpg'
                   fill
                   alt='Pool Deck and Retaining Wall'
-                  className="object-cover"
+                  className="object-cover hover:opacity-70"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <hr className="border-2 fade-hidden mb-3 border-darkGreen"/>
+
+        {/* Summer Pruning */}
+        <section className="flex flex-col fade-hidden mb-3">
+          <div>
+            <div>
+              <h1 className="text-2xl font-medium pl-2 text-center md:text-left">
+                Summer Pruning
+              </h1>
+            </div>
+          </div>
+          <div className="grid sm:grid-cols-1 md:grid-cols-3">
+            <div className="p-2">
+              <div className="text-center md:text-left">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, quo. Nesciunt error ullam debitis cupiditate odio veniam sunt quisquam cum ipsa atque, voluptas neque iste perspiciatis laudantium. Iste, ex mollitia?
+                </p>
+              </div>
+            </div>
+            <div className="p-2 flex items-center">
+              <div className="relative w-full h-[300px] hover:cursor-zoom-in" onClick={() => openModal(4)}>
+                <Image
+                  src='/images/landscape/seasonal_services/summer_pruning/front_walkway.jpg'
+                  fill
+                  alt='Pool Deck and Retaining Wall'
+                  className="object-cover hover:opacity-70"
+                />
+              </div>
+            </div>
+            <div className="p-2 flex items-center">
+              <div className="relative w-full h-[300px] hover:cursor-zoom-in" onClick={() => openModal(5)}>
+                <Image
+                  src='/images/landscape/seasonal_services/summer_pruning/front_walk_with_bushes.jpg'
+                  fill
+                  alt='Pool Deck and Retaining Wall'
+                  className="object-cover hover:opacity-70"
                 />
               </div>
             </div>
@@ -161,31 +188,31 @@ export default function SeasonalServices() {
               </div>
             </div>
             <div className="p-2 flex items-center">
-              <div className="relative w-full h-[300px]">
+              <div className="relative w-full h-[300px] hover:cursor-zoom-in" onClick={() => openModal(2)}>
                 <Image
-                  src='/images/services-banner-cropped.png'
+                  src='/images/landscape/seasonal_services/leaf/leaf_blowing.jpg'
                   fill
                   alt='Pool Deck and Retaining Wall'
-                  className="object-cover"
+                  className="object-cover hover:opacity-70"
                 />
               </div>
             </div>
             <div className="p-2 flex items-center">
-              <div className="relative w-full h-[300px]">
+              <div className="relative w-full h-[300px] hover:cursor-zoom-in" onClick={() => openModal(3)}>
                 <Image
-                  src='/images/services-banner-cropped.png'
+                  src='/images/landscape/seasonal_services/leaf/leaf_cleanup.jpg'
                   fill
                   alt='Pool Deck and Retaining Wall'
-                  className="object-cover"
+                  className="object-cover hover:opacity-70"
                 />
               </div>
             </div>
           </div>
         </section>
         
-        <hr className="border-2 fade-hidden mb-3 border-darkGreen"/>
+        {/* <hr className="border-2 fade-hidden mb-3 border-darkGreen"/>
 
-        {/* Snow Removal */}
+        Snow Removal
         <section className="flex flex-col fade-hidden mb-3">
           <div>
             <div>
@@ -203,70 +230,67 @@ export default function SeasonalServices() {
               </div>
             </div>
             <div className="p-2 flex items-center">
-              <div className="relative w-full h-[300px]">
+              <div className="relative w-full h-[300px] hover:cursor-zoom-in" onClick={() => openModal(fixME)}>
                 <Image
-                  src='/images/services-banner-cropped.png'
+                  src='/images/landscape/seasonal_services/leaf/leaf_cleanup.jpg'
                   fill
                   alt='Pool Deck and Retaining Wall'
-                  className="object-cover"
+                  className="object-cover hover:opacity-70"
                 />
               </div>
             </div>
             <div className="p-2 flex items-center">
-              <div className="relative w-full h-[300px]">
+              <div className="relative w-full h-[300px] hover:cursor-zoom-in" onClick={() => openModal(fixME)}>
                 <Image
                   src='/images/services-banner-cropped.png'
                   fill
                   alt='Pool Deck and Retaining Wall'
-                  className="object-cover"
+                  className="object-cover hover:opacity-70"
                 />
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
 
-        <hr className="border-2 fade-hidden mb-3 border-darkGreen"/>
-
-        {/* Summer Pruning */}
-        <section className="flex flex-col fade-hidden mb-3">
-          <div>
-            <div>
-              <h1 className="text-2xl font-medium pl-2 text-center md:text-left">
-                Summer Pruning
-              </h1>
-            </div>
-          </div>
-          <div className="grid sm:grid-cols-1 md:grid-cols-3">
-            <div className="p-2">
-              <div className="text-center md:text-left">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, quo. Nesciunt error ullam debitis cupiditate odio veniam sunt quisquam cum ipsa atque, voluptas neque iste perspiciatis laudantium. Iste, ex mollitia?
-                </p>
-              </div>
-            </div>
-            <div className="p-2 flex items-center">
-              <div className="relative w-full h-[300px]">
-                <Image
-                  src='/images/services-banner-cropped.png'
-                  fill
-                  alt='Pool Deck and Retaining Wall'
-                  className="object-cover"
-                />
-              </div>
-            </div>
-            <div className="p-2 flex items-center">
-              <div className="relative w-full h-[300px]">
-                <Image
-                  src='/images/services-banner-cropped.png'
-                  fill
-                  alt='Pool Deck and Retaining Wall'
-                  className="object-cover"
-                />
-              </div>
-            </div>
-          </div>
-        </section>
       </div>
+
+      {/* Lightbox Modal */}
+            {isOpen && (
+              <div className="hover:cursor-zoom-out fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50" onClick={closeModal}>
+                <button
+                  className="absolute top-5 right-5 text-white text-3xl hover:opacity-70"
+                  onClick={closeModal}
+                >
+                  ✖
+                </button>
+      
+                {/* Previous Button */}
+                <div
+                  className="absolute left-5 cursor-pointer text-white text-3xl p-3 hover:opacity-70"
+                  onClick={(e) => { e.stopPropagation(); prevImage(); }}
+                >
+                  Previous
+                </div>
+      
+                {/* Enlarged Image */}
+                <Image 
+                  src={enlargedImages[enlargedImageIndex]} 
+                  width={2000} 
+                  height={1500} 
+                  alt={`Gallery image ${enlargedImageIndex + 1}`} 
+                  className="rounded-lg hover:cursor-pointer"
+                  onClick={(e) => { e.stopPropagation(); nextImage(); }}
+                />
+      
+                {/* Next Button */}
+                <div
+                  className="absolute right-5 cursor-pointer text-white text-3xl p-3 hover:opacity-70"
+                  onClick={(e) => { e.stopPropagation(); nextImage(); }}
+                >
+                  Next
+                </div>
+              </div>
+            )}
     </div>
   );
 }
