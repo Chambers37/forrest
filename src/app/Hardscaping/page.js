6222dbefd6bfc67d0def4f718bf91c5c2c22cc16
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2025-04-12 23:18:12 -0400

    fixed image alt text for seo

--- src/app/Hardscaping/page.js
+++ src/app/Hardscaping/page.js
@@ -105,7 +105,7 @@ export default function Hardscaping() {
                       src={'/images/hardscaping/PoolDecks/FullPoolDeck.jpg'} 
                       fill
                       className="object-cover"
-                      alt='fix me test'
+                      alt='Beautiful pool deck with natural stone retaining wall and elevated seating area'
                     />
                     
                   </Link>
